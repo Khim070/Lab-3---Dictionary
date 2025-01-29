6695e9694f0e064e79685ad05534f378014560e7
Khim070 <Vakhiml60@gmail.com>
2025-01-29 19:05:33 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dictionary</title>
+</head>
+
+<body>
+    <?php
+    $wordGroups = [];
+    $dictionary = fopen("Oxford English Dictionary.txt", "r") or die("Unable to open the file");
+    while (!feof($dictionary)) {
+        $word = fgets($dictionary);
+        $firstletter = strtoupper($word[0]);
+        if (preg_match('/^[A-Z]$/', $firstletter)) {
+            $wordGroups[$firstletter][] = $word;
+        }
+    }
+
+    fclose($dictionary);
+
+    foreach ($wordGroups as $letter => $words) {
+        $filename = "Dictionary/" . $letter . ".txt";
+        $file = fopen($filename, "w");
+
+        foreach ($words as $word) {
+            fwrite($file, $word . "\n");
+        }
+
+        fclose($file);
+    }
+
+    ?>
+    <form action="" method="post">
+        <label>Enter a word:</label>
+        <input type="text" id="word" name="txtword" required>
+        <input type="submit" value="Search">
+    </form>
+    <?php
+        if (isset($_POST['txtword'])) {
+            $firstWord = trim($_POST['txtword']);
+            $eachWord = strtoupper($firstWord[0]);
+            $eachFilename = fopen("Dictionary/$eachWord.txt", "r");
+            $wordFound = false;
+            while(!feof($eachFilename)){
+                $eachLine = fgets($eachFilename);
+
+                if (stripos($eachLine, $firstWord) === 0 || stripos($eachLine, $firstWord) === 0) {
+                    echo "<p><strong>Word:</strong> $eachLine</p>";
+                    $wordFound = true;
+                    break;
+                }
+            }
+            fclose($eachFilename);
+
+            if (!$wordFound) {
+                echo "<p>No such word found.</p>";
+            }
+        }
+    ?>
+</body>
+
+</html>
